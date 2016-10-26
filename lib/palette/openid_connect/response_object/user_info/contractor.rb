74dc38e477c84b38d00e1c40e789cb8153b08f38
Yuki Masutomi <tommy@machikoe.com>
2016-10-26 19:07:54 +0900

    rm contract info from tenant, contract

--- lib/palette/openid_connect/response_object/user_info/contractor.rb
+++ lib/palette/openid_connect/response_object/user_info/contractor.rb
@@ -3,23 +3,24 @@ module Palette
     class ResponseObject
       class UserInfo
         class Contractor < ::OpenIDConnect::ConnectObject
-          attr_optional :contracts
+          # TODO future
+          # attr_optional :contracts
           attr_optional :identity_code
           attr_optional :labels
           validate :require_at_least_one_attributes
 
-          def contracts=(array_of_hash_or_contracts)
-            return nil if array_of_hash_or_contracts.nil?
-
-            @contracts = array_of_hash_or_contracts.map do |hash_or_contract|
-              case hash_or_contract
-              when Hash
-                Contract.new hash_or_contract
-              when Contract
-                hash_or_contract
-              end
-            end
-          end
+          # def contracts=(array_of_hash_or_contracts)
+          #   return nil if array_of_hash_or_contracts.nil?
+          #
+          #   @contracts = array_of_hash_or_contracts.map do |hash_or_contract|
+          #     case hash_or_contract
+          #     when Hash
+          #       Contract.new hash_or_contract
+          #     when Contract
+          #       hash_or_contract
+          #     end
+          #   end
+          # end
 
           def labels=(array_or_string)
             @labels = case array_or_string
